6bea5a7d646c2df2db07745c21e823cbfaab1614
Dionshameen034 <dionshameen705@gmail.com>
2024-03-11 13:23:05 +0300

    Initial Commit

---
+++ src/intro.kt
@@ -0,0 +1,12 @@
+fun main(args: Array<String>) {
+    println("Welcome to Kotlin Programming")
+    println(5+10)
+
+    val x = 10
+    val y = 15
+
+    println(x + y)
+
+    println("There are $x exes")
+    println("There are ${y+1} bottles")
+}
